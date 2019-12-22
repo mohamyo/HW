5bc96326f2e7b02d6b0a7683153eca6a6724c35c
Bahaa Abu Rabiah <bha3142@gmail.com>
2019-12-22 20:21:05 +0200

    Create workingStudent.h

---
+++ workingStudent.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Employee.h"
+#include "Student.h"
+#include "Person.h"
+using namespace std;
+
+class workingStudent : public student, public employee {
+protected:
+	bool same_institute;
+
+public:
+	workingStudent(char*name, long id, int age, int average, char* institute, float sallary, bool same_institute) :student(name, id, age, average, institute), employee(name, id, age, sallary) {
+		this->same_institute = same_institute;
+	}
+	
+};
